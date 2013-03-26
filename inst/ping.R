fcd78c4bc0f5623723942098b81b7ec4f545a606
Wush Wu <wush@bridgewell.com>
2013-03-26 16:26:50 +0800

    update

--- inst/ping.R
+++ inst/ping.R
@@ -1,6 +1,8 @@
+#! /usr/bin/Rscript
 cat(Sys.getpid())
 capture.output({
   suppressWarnings(library(RzmqJobQueue, quietly=TRUE))
   argv <- commandArgs(trailingOnly=TRUE)
+  stopifnot(length(argv) == 3)
   while(TRUE) ping(argv[1], argv[2], as.integer(argv[3]))
 })

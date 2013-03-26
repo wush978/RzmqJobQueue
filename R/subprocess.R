b95848f14eee6e6c57ea4e16ddc40a5224df0d20
Wush Wu <wush@bridgewell.com>
2013-03-26 16:29:48 +0800

    suppressWarnings: incomplete final line

--- R/subprocess.R
+++ R/subprocess.R
@@ -4,7 +4,7 @@ open_subprocess <- function(script.name, ...) {
   system2("Rscript", args = c(system.file(script.name, package="RzmqJobQueue"), unlist(list(...))), wait=FALSE, stdout = ping.stdout)
   while(!file.exists(ping.stdout)) next
   while(file.info(ping.stdout)$size == 0) next
-  ping.pid <- as.integer(readLines(ping.stdout))
+  suppressWarnings(ping.pid <- as.integer(readLines(ping.stdout)))
   stopifnot(length(ping.pid) > 0)
   stopifnot(ping.pid > 0)
   return(ping.pid)

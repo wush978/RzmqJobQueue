b95562f28b4c2036370ddd4de2795b5b69b1c55d
Wush Wu <wush@bridgewell.com>
2013-03-26 16:27:28 +0800

    checking if child-process is cleaned

--- tests/zmqSapply.tests.R
+++ tests/zmqSapply.tests.R
@@ -3,3 +3,4 @@ zmqSapply("tcp://*:12345", as.list(1:10), function(n) {
   if (runif(1) < 0.5) stop("testing error handling")
   letters[1:n]
   })
+stopifnot(length(system("ps -ef | grep do_job.R", intern = TRUE)) == 2)

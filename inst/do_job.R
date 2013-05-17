23e25f0e582e8dc4974000e892d9e97732edeefa
Wush Wu <wush@bridgewell.com>
2013-05-17 18:09:29 +0800

    modify stdout of workers spawn by do_job

--- inst/do_job.R
+++ inst/do_job.R
@@ -1,9 +1,10 @@
 #! /usr/bin/Rscript
 cat(Sys.getpid())
-capture.output({
-  suppressPackageStartupMessages(library(RzmqJobQueue))
-  library(RzmqJobQueue)
-  argv <- commandArgs(trailingOnly=TRUE)
-  init_worker(argv[1], argv[2])
-  while(TRUE) do_job(argv[1], argv[2])
-})
+
+suppressPackageStartupMessages(library(RzmqJobQueue))
+library(RzmqJobQueue)
+argv <- commandArgs(trailingOnly=TRUE)
+init_worker(argv[1], argv[2])
+while(TRUE) {
+  do_job(argv[1], argv[2])
+}
